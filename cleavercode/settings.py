06e08a2742145bb88cfa5b24076513bfec809b8e
koustav <koustavroy88@gmail.com>
2025-08-21 23:58:46 +0300

    Render: Postgres + static + hosts config all

--- cleavercode/settings.py
+++ cleavercode/settings.py
@@ -25,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p#$6(n-v%8_pczb0paxa1$utmzkno8@mwr#jo1x(m(opmvrueq'
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
